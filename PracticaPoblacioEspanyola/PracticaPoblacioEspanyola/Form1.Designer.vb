f1c7e79b83a20a772f76a89178b7eba7b914deca
mariusGVD <marius.hagiu.91@gmail.com>
2016-05-31 14:15:29 +0200

    example_program_vb.net-.txt-file

---
+++ PracticaPoblacioEspanyola/PracticaPoblacioEspanyola/Form1.Designer.vb
@@ -0,0 +1,327 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.ListView1 = New System.Windows.Forms.ListView()
+        Me.ColumnHeader1 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader2 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader3 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader4 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.RadioButton2 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton1 = New System.Windows.Forms.RadioButton()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.ComboBox2 = New System.Windows.Forms.ComboBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.GroupBox3 = New System.Windows.Forms.GroupBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.GroupBox3.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(6, 56)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(156, 21)
+        Me.ComboBox1.Sorted = True
+        Me.ComboBox1.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(6, 40)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(51, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Provincia"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(401, 16)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(113, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Dades de la provincia:"
+        '
+        'ListView1
+        '
+        Me.ListView1.Columns.AddRange(New System.Windows.Forms.ColumnHeader() {Me.ColumnHeader1, Me.ColumnHeader2, Me.ColumnHeader3, Me.ColumnHeader4})
+        Me.ListView1.FullRowSelect = True
+        Me.ListView1.Location = New System.Drawing.Point(404, 38)
+        Me.ListView1.Name = "ListView1"
+        Me.ListView1.Size = New System.Drawing.Size(439, 176)
+        Me.ListView1.TabIndex = 4
+        Me.ListView1.UseCompatibleStateImageBehavior = False
+        Me.ListView1.View = System.Windows.Forms.View.Details
+        '
+        'ColumnHeader1
+        '
+        Me.ColumnHeader1.Text = "Municipis"
+        Me.ColumnHeader1.Width = 170
+        '
+        'ColumnHeader2
+        '
+        Me.ColumnHeader2.Text = "Població"
+        Me.ColumnHeader2.Width = 100
+        '
+        'ColumnHeader3
+        '
+        Me.ColumnHeader3.Text = "Homes"
+        Me.ColumnHeader3.Width = 80
+        '
+        'ColumnHeader4
+        '
+        Me.ColumnHeader4.Text = "Dones"
+        Me.ColumnHeader4.Width = 80
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.RadioButton2)
+        Me.GroupBox1.Controls.Add(Me.RadioButton1)
+        Me.GroupBox1.Controls.Add(Me.Label7)
+        Me.GroupBox1.Controls.Add(Me.Button1)
+        Me.GroupBox1.Controls.Add(Me.Label6)
+        Me.GroupBox1.Controls.Add(Me.ListBox1)
+        Me.GroupBox1.Controls.Add(Me.TextBox1)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.ComboBox2)
+        Me.GroupBox1.Location = New System.Drawing.Point(13, 248)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(395, 202)
+        Me.GroupBox1.TabIndex = 5
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Poblacions depenen del numero d'habitants "
+        '
+        'RadioButton2
+        '
+        Me.RadioButton2.AutoSize = True
+        Me.RadioButton2.Location = New System.Drawing.Point(89, 34)
+        Me.RadioButton2.Name = "RadioButton2"
+        Me.RadioButton2.Size = New System.Drawing.Size(56, 17)
+        Me.RadioButton2.TabIndex = 11
+        Me.RadioButton2.TabStop = True
+        Me.RadioButton2.Text = "Dones"
+        Me.RadioButton2.UseVisualStyleBackColor = True
+        '
+        'RadioButton1
+        '
+        Me.RadioButton1.AutoSize = True
+        Me.RadioButton1.Location = New System.Drawing.Point(11, 34)
+        Me.RadioButton1.Name = "RadioButton1"
+        Me.RadioButton1.Size = New System.Drawing.Size(58, 17)
+        Me.RadioButton1.TabIndex = 10
+        Me.RadioButton1.TabStop = True
+        Me.RadioButton1.Text = "Homes"
+        Me.RadioButton1.UseVisualStyleBackColor = True
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(86, 69)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(101, 13)
+        Me.Label7.TabIndex = 9
+        Me.Label7.Text = "Nombre d'habitants:"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(138, 146)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 8
+        Me.Button1.Text = "Buscar"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(250, 33)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(62, 13)
+        Me.Label6.TabIndex = 7
+        Me.Label6.Text = "Poblacions:"
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(253, 49)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(120, 147)
+        Me.ListBox1.TabIndex = 6
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(89, 85)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(75, 20)
+        Me.TextBox1.TabIndex = 5
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(8, 68)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(51, 13)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "Condició:"
+        '
+        'ComboBox2
+        '
+        Me.ComboBox2.FormattingEnabled = True
+        Me.ComboBox2.Location = New System.Drawing.Point(6, 84)
+        Me.ComboBox2.Name = "ComboBox2"
+        Me.ComboBox2.Size = New System.Drawing.Size(64, 21)
+        Me.ComboBox2.TabIndex = 2
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.Label4)
+        Me.GroupBox2.Controls.Add(Me.TextBox2)
+        Me.GroupBox2.Controls.Add(Me.ListView1)
+        Me.GroupBox2.Controls.Add(Me.ComboBox1)
+        Me.GroupBox2.Controls.Add(Me.Label2)
+        Me.GroupBox2.Controls.Add(Me.Label1)
+        Me.GroupBox2.Location = New System.Drawing.Point(13, 12)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(873, 220)
+        Me.GroupBox2.TabIndex = 6
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Dades sobre les provincies"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(201, 39)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(116, 13)
+        Me.Label4.TabIndex = 6
+        Me.Label4.Text = "Nombre de poblacions:"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(221, 55)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.ReadOnly = True
+        Me.TextBox2.Size = New System.Drawing.Size(73, 20)
+        Me.TextBox2.TabIndex = 5
+        Me.TextBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'GroupBox3
+        '
+        Me.GroupBox3.Controls.Add(Me.Button2)
+        Me.GroupBox3.Controls.Add(Me.TextBox3)
+        Me.GroupBox3.Controls.Add(Me.Label5)
+        Me.GroupBox3.Location = New System.Drawing.Point(524, 248)
+        Me.GroupBox3.Name = "GroupBox3"
+        Me.GroupBox3.Size = New System.Drawing.Size(362, 181)
+        Me.GroupBox3.TabIndex = 7
+        Me.GroupBox3.TabStop = False
+        Me.GroupBox3.Text = "Cercador de provincies"
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(221, 107)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(111, 42)
+        Me.Button2.TabIndex = 7
+        Me.Button2.Text = "Cerca provincia"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(19, 66)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(159, 20)
+        Me.TextBox3.TabIndex = 6
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(16, 45)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(159, 13)
+        Me.Label5.TabIndex = 5
+        Me.Label5.Text = "Introdueix el nom de la pablació:"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(922, 462)
+        Me.Controls.Add(Me.GroupBox3)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.GroupBox3.ResumeLayout(False)
+        Me.GroupBox3.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents ComboBox1 As System.Windows.Forms.ComboBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents ListView1 As System.Windows.Forms.ListView
+    Friend WithEvents ColumnHeader1 As System.Windows.Forms.ColumnHeader
+    Friend WithEvents ColumnHeader2 As System.Windows.Forms.ColumnHeader
+    Friend WithEvents ColumnHeader3 As System.Windows.Forms.ColumnHeader
+    Friend WithEvents ColumnHeader4 As System.Windows.Forms.ColumnHeader
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents ComboBox2 As System.Windows.Forms.ComboBox
+    Friend WithEvents ListBox1 As System.Windows.Forms.ListBox
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents GroupBox2 As System.Windows.Forms.GroupBox
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents GroupBox3 As System.Windows.Forms.GroupBox
+    Friend WithEvents TextBox3 As System.Windows.Forms.TextBox
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents RadioButton2 As System.Windows.Forms.RadioButton
+    Friend WithEvents RadioButton1 As System.Windows.Forms.RadioButton
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+
+End Class
---
+++ PracticaPoblacioEspanyola/PracticaPoblacioEspanyola/Form1.vb
@@ -0,0 +1,236 @@
+﻿Imports System.IO
+Imports System.Text
+'H.M.C.
+Public Class Form1
+    Dim sre As IO.StreamReader = Nothing
+    Dim linea As String
+    Dim arrDades() As String
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        
+
+        sre = New IO.StreamReader("pobmun13.txt", System.Text.Encoding.Default)
+
+        Do Until sre.Peek = -1
+            linea = sre.ReadLine()
+            arrDades = Split(linea, vbTab)
+
+            If ComboBox1.Items.Contains(arrDades(1)) = False Then
+
+                ComboBox1.Items.Add(arrDades(1))
+
+            End If
+        Loop
+
+        ComboBox2.Items.Add("<")
+        ComboBox2.Items.Add("=")
+        ComboBox2.Items.Add(">")
+
+
+
+    End Sub
+
+   
+    Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox1.SelectedIndexChanged
+
+        sre = New IO.StreamReader("pobmun13.txt", System.Text.Encoding.Default)
+        Refresh()
+
+        Do Until sre.Peek = -1
+            linea = sre.ReadLine()
+            arrDades = Split(linea, vbTab)
+
+            Dim provincia As String
+            provincia = arrDades(1).ToString()
+            Dim numPob As Integer
+
+            If ComboBox1.SelectedItem.ToString = provincia Then
+                For Each provincia In arrDades(1).ToString
+                    numPob = ListView1.Items.Count
+                    TextBox2.Text = numPob.ToString()
+                    Dim item1 As ListViewItem = ListView1.FindItemWithText(arrDades(3)) ' Busquem es el poble ja existeix en la llista per no repetir-lo
+                    If (item1 Is Nothing) Then
+                        Dim i As Integer ' index
+                        Dim str(4) As String ' creem un array de strings de 4 columnes
+                        Dim itm As ListViewItem
+
+                        For i = 0 To 3
+                            str(i) = arrDades(i + 3)
+                        Next
+                        itm = New ListViewItem(str) ' insertem al listview l'array de strings
+                        ListView1.Items.Add(itm) ' afegim items linea per linea
+                    End If
+                Next
+            End If
+        Loop
+
+
+    End Sub
+
+    Public Overrides Sub Refresh()
+        ListView1.Items.Clear()
+    End Sub
+
+    
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        ListBox1.Items.Clear()
+        sre = New IO.StreamReader("pobmun13.txt", System.Text.Encoding.Default)
+
+        ' Verifiquem si s'han completat els camps de les condicions
+        If (String.IsNullOrEmpty(ComboBox2.SelectedItem) OrElse
+            String.IsNullOrEmpty(TextBox1.Text) OrElse
+            (RadioButton1.Checked = False And RadioButton2.Checked = False)) Then
+
+            MessageBox.Show("Introdueix totes les condicions de busqueda!")
+            ' Si els comps no estan complets sortim
+            Return
+        End If
+
+        ' Variable de la condició <, >, =
+        Dim condicio As String
+        condicio = ComboBox2.SelectedItem.ToString()
+
+        ' Variable nombre habitants
+        Dim nombrehabitants As Integer
+        nombrehabitants = TextBox1.Text
+
+        ' Variable sexe. Es guarda am el valor 1 si es home i 2 si es dona
+        Dim sex As Integer
+        If RadioButton1.Checked = True Then
+            sex = 1
+        End If
+        If RadioButton2.Checked = True Then
+            sex = 2
+        End If
+
+        Do Until sre.Peek = -1
+            linea = sre.ReadLine()
+            arrDades = Split(linea, vbTab)
+            If (String.IsNullOrEmpty(arrDades(4))) Then Continue Do
+
+            ' Guardo el nr. d'habitants i sexe en un vector amb els indicis 1 i 2 per asociar-lo amb el sexe 1=home, 2=dona
+            Dim num(2) As Integer
+
+
+            'Convertim el num d'habitants que extreiem del array a integer pq esta com a string i reemplaçem el "." per res
+            '(alguns nr. tenen "." per agrupar les unitats)
+            'Homes
+            num(1) = Convert.ToInt32(arrDades(5).Replace(".", ""))
+            'Dones
+            num(2) = Convert.ToInt32(arrDades(6).Replace(".", ""))
+
+            'Variable poblacio que conte la columna del nom de les poblacions
+            Dim poblacio As String
+            poblacio = arrDades(3)
+
+            If (condicio = ">") Then ' Mes gran
+                If (num(sex) > nombrehabitants) Then
+                    ListBox1.Items.Add(poblacio)
+                    ListBox1.Sorted = True
+                End If
+            ElseIf (condicio = "=") Then ' Igual
+                If (num(sex) = nombrehabitants) Then
+                    ListBox1.Items.Add(poblacio)
+                    ListBox1.Sorted = True
+                End If
+            Else ' Mes petit
+                If (num(sex) < nombrehabitants) Then
+                    ListBox1.Items.Add(poblacio)
+                    ListBox1.Sorted = True
+                End If
+            End If
+
+        Loop
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+        sre = New IO.StreamReader("pobmun13.txt", System.Text.Encoding.Default)
+
+        ' Verifiquem si s'han completat els camps de les condicions
+        If (String.IsNullOrEmpty(TextBox3.Text)) Then
+
+            MessageBox.Show("Introdueix la població!")
+            ' Si el camp del textbox 3 esta en buit o null sortim
+            Return
+        End If
+
+        'Variable poble= nom del poble introduit pel usuari
+        Dim poble As String
+        ' Cambiem les majuscules per minuscules per controlar millor el text introduit
+        poble = TextBox3.Text.ToString().ToLower()
+
+        ' Variable que indica si s'ha trobat algun poble
+        Dim trobat As Boolean
+        trobat = False
+
+        Do Until sre.Peek = -1
+            linea = sre.ReadLine()
+            arrDades = Split(linea, vbTab)
+
+            'Variable per guardar el nom del poble del fitxer txt
+            Dim pobletxt As String
+            ' Combio majuscules per minuscules perque l'usuari pugui introduir el nom tal com vol am majuscules o minuscules
+            'i que el programa el trobi igualment en el fitxer. Exemple : "Balsa de Ves" que pertany a la provincia Albacete
+            'pot ser escrit pel usuari com a : Balsa de ves, Balsa DE VES, etc... el programa convertira les maj en min i trobara igualment el resultat
+            pobletxt = arrDades(3).ToString().ToLower()
+
+            If (poble = pobletxt) Then
+                ' Daca a fost gasit cel putin un rezultat setezi variabila ca true
+                trobat = True
+                MessageBox.Show("El poble " + pobletxt.ToUpper() + " forma part de la provincia " + arrDades(1).ToString() + " .", "Informació")
+            End If
+
+        Loop
+        TextBox3.Text = ""
+
+        ' si es false vol dir que no existeix en el fitx.
+        If (Not trobat) Then
+            MessageBox.Show("El poble " + poble.ToUpper() + " no pertany a cap provincia o es incorrecte!!! ", "Informació")
+        End If
+
+
+    End Sub
+
+    Private Sub TextBox3_KeyPress(sender As Object, e As KeyPressEventArgs) Handles TextBox3.KeyPress
+        'corverteix el primer caracter a majuscula
+        If TextBox3.SelectionStart = 0 Then e.KeyChar = CChar(e.KeyChar.ToString.ToUpper)
+
+        'valido el textbox perque no es puguin introduir numeros
+        If Char.IsLetter(e.KeyChar) Then
+            e.Handled = False
+        ElseIf Char.IsControl(e.KeyChar) Then
+            e.Handled = False
+        ElseIf Char.IsSeparator(e.KeyChar) Then
+            e.Handled = False
+        Else
+            e.Handled = True
+        End If
+
+    End Sub
+
+    Private Sub TextBox1_KeyPress(sender As Object, e As KeyPressEventArgs) Handles TextBox1.KeyPress
+
+        'valido el textbox perque sol es puguin introduir numeros
+        If Char.IsDigit(e.KeyChar) Then
+            e.Handled = False
+        ElseIf Char.IsControl(e.KeyChar) Then
+            e.Handled = False
+        ElseIf Char.IsSymbol(e.KeyChar) Then
+            e.Handled = False
+        ElseIf Char.IsSeparator(e.KeyChar) Then
+            e.Handled = False
+        ElseIf Char.IsWhiteSpace(e.KeyChar) Then
+            e.Handled = False
+        Else
+            e.Handled = True
+        End If
+
+        'Si en el textbox trobem un espai entre numeros el corregim borran-lo
+        Me.TextBox1.Text = Trim(Replace(Me.TextBox1.Text, "  ", " "))
+        TextBox1.Select(TextBox1.Text.Length, 0)
+    End Sub
+End Class
